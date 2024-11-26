b69e50df405bac0a5ef1da9bb2b20be5ec418f15
rowan04 <rowan.moss@stfc.ac.uk>
2024-11-26 18:55:42 +0000

    Add get_attachments function in routers/

--- object_storage_api/routers/attachment.py
+++ object_storage_api/routers/attachment.py
@@ -6,7 +6,7 @@ service.
 import logging
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Query, status
 
 from object_storage_api.schemas.attachment import AttachmentPostResponseSchema, AttachmentPostSchema
 from object_storage_api.services.attachment import AttachmentService
@@ -33,3 +33,20 @@ def create_attachment(
     logger.debug("Attachment data: %s", attachment)
 
     return attachment_service.create(attachment)
+
+@router.get(
+    path="",
+    summary="Get attachments",
+    response_description="List of attachments",
+)
+def get_attachments(
+    attachment_service: AttachmentServiceDep,
+    entity_id: Annotated[Optional[str], Query(description="Filter attachments by entity ID")] = None,
+) -> list[AttachmentPostResponseSchema]:
+    # pylint: disable=missing-function-docstring
+    logger.info("Getting attachments")
+
+    if entity_id is not None:
+        logger.debug("Entity ID filter: '%s'", entity_id)
+
+    return attachment_service.list(entity_id)
