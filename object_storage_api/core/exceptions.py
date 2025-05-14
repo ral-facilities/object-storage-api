f45fe270e36a63e6de88b08f976087610b0ae5a8
Joel Davies <joel.davies@stfc.ac.uk>
2025-05-14 08:29:08 +0000

    Pull base exception from common repo #160

--- object_storage_api/core/exceptions.py
+++ object_storage_api/core/exceptions.py
@@ -9,33 +9,7 @@ Module for custom exception classes.
 from typing import Optional
 
 from fastapi import status
-
-
-class BaseAPIException(Exception):
-    """Base exception for API errors."""
-
-    # Status code to return if this exception is raised
-    status_code: int
-
-    # Generic detail of the exception (That may be returned in a response)
-    response_detail: str
-
-    detail: str
-
-    def __init__(self, detail: str, response_detail: Optional[str] = None):
-        """
-        Initialise the exception.
-
-        :param detail: Specific detail of the exception (just like Exception would take - this will only be logged
-                       and not returned in a response).
-        :param response_detail: Generic detail of the exception that will be returned in a response.
-        """
-        super().__init__(detail)
-
-        self.detail = detail
-
-        if response_detail is not None:
-            self.response_detail = response_detail
+from ims_common.exceptions import BaseAPIException
 
 
 class DatabaseError(BaseAPIException):
